69717304561a560c75d821f0cafdb2617f0fb717
Socrates <nsocrates.dev@gmail.com>
2019-03-05 14:50:19 -0800

    PDP : Remove useless line

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -2142,7 +2142,6 @@ theme.ProductForm = function (context, events) {
 
         // Select first available variant if top row of options changed
         if (parseInt(option_position, 10) === 1) {
-          var correspondingVariants = product.variants.filter(variant => variant.option1 === element.value)
           var correspondingVariants = product.variants.reduce((acc, cur, i) => {
             if (cur.option1 === element.value) {
               acc.selected = cur
