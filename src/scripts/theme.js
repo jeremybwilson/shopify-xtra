7f80314a3d6250dc04a1949e97500bf1800ff59f
Rahul M <rahulm@shivlab.com>
2019-07-22 18:01:05 +0530

    PDM-shivlab-new
    change in stock and some design

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -815,11 +815,6 @@ a.prototype[n]=function(){var n=arguments;return e&&!this.__chain__?t.apply(this
  */
 // (function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
 
-/*
- * ----------------------------------------------------------------------------------------------------
- * DISCOUNT ENGINE (React Component)
- * --------------------------------------------------------------------------------------------------- */
-require( './react-components/discounts/DiscountParent.js' );
 
 window.theme = window.theme || {}
 
@@ -830,6 +825,13 @@ theme.Utils = (function () {
     },
   }
 })();
+
+/*
+ * ----------------------------------------------------------------------------------------------------
+ * DISCOUNT ENGINE (React Component)
+ * --------------------------------------------------------------------------------------------------- */
+require( './react-components/discounts/DiscountParent.js' );
+
 /*============================================================================
   Sections
 ==============================================================================*/
